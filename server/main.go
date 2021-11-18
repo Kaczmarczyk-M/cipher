c65406d7c05be6410462cba4f8f8f84fa942ef34
Michal Kaczmarczyk <m01kaczmarczyk@gmail.com>
2021-11-18 14:25:12 +0100

    read old, save new messages to json file

--- server/main.go
+++ server/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"strings"
+	"time"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
+
+type uRequest struct {
+	Id       int    `json:"id"`
+	Req      string `json:"req"`
+	Dateunix int64  `json:"date"`
+}
+
+var AllRequests []uRequest
+
+const placeToSaveData string = "../data.json"
 
 var g string
 var allChar = [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", "/", "?", "!", "@", "%", "(", ")", " "}
@@ -49,7 +62,35 @@ func caesarcipher(g string) (string, int) {
 	return strings.Join(workingHashedPhrase, ""), jump
 }
 
+func addRequestToJson(nameOfReq string, reqs []uRequest) {
+	// check if list isnt out of range and calculate id
+	var whichId int
+	if len(reqs) > 0 {
+		whichId = reqs[len(reqs)-1].Id + 1
+	} else {
+		whichId = 0
+	}
+
+	AllRequests = append(AllRequests, uRequest{
+		Id:       whichId,
+		Req:      nameOfReq,
+		Dateunix: time.Now().Unix(),
+	})
+
+	file, _ := json.MarshalIndent(AllRequests, "", " ")
+	_ = ioutil.WriteFile(placeToSaveData, file, 0644)
+}
+
 func main() {
+	//read previous messages
+	file, err := ioutil.ReadFile(placeToSaveData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var holdDataFromFile []uRequest
+	json.Unmarshal(file, &holdDataFromFile)
+	AllRequests = append(AllRequests, holdDataFromFile...)
+	//start serving
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
 	api := r.Group("/api")
@@ -62,6 +103,7 @@ func main() {
 		if c.FullPath() == "/api/tobehashed" {
 			g = c.Request.URL.Query()["inputValue"][0]
 			list, jump := caesarcipher(g)
+			addRequestToJson(g, AllRequests)
 			c.JSON(200, gin.H{"msgCaesar": list, "jump": jump, "md5": md5.Sum([]byte(g))})
 			fmt.Println(md5.Sum([]byte(g)))
 		} else {
