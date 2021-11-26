4387f5bf67f505908671173c6bf8c0b8bd3521ff
Michal Kaczmarczyk <m01kaczmarczyk@gmail.com>
2021-11-26 20:08:53 +0100

    added sign-in page and react router. TODO: constant colorway, auth on server side or with react router, sign-up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Content from "./components/content";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./components/sign-in";
+import * as React from "react";
 
+function home() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Content />
+    </header>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Content />
-      </header>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/home"element={home()}/>
+      </Routes>
     </div>
   );
 }
